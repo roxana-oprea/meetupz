e7171537544adfbd4d15719c4dc06e467186bcf6
roxana-oprea <oprearoxanaadolfina@gmail.com>
2018-09-10 21:14:01 +0300

    add meetups component

---
+++ src/components/Meetups.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Meetups extends Component {
+  render() {
+    return (
+      <div>
+        <h1>meetups</h1>
+      </div>
+    )
+  }
+}
+
+
+export default Meetups;
