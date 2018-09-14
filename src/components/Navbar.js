cb4469accd16708b17b539ae5038c5a9db3a7575
roxana-oprea <oprearoxanaadolfina@gmail.com>
2018-09-14 20:11:46 +0300

    add about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,7 @@ class Navbar extends Component {
             <ul className="sidenav" id="main-menu">
               <li><Link to="/"><i className="fa fa-users"/>Meetups</Link></li>
               <li><Link to="/meetups/add"><i className="fa fa-plus"/>Add Meetup</Link></li>
+              <li><Link to="/about"><i className="fa fa-question-circle"/>About</Link></li>
             </ul>
           </div>
         </nav>
